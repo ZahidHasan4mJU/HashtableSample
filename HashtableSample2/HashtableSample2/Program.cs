08b72a26d621d6a2359aa99f7a7fcf718063c27f
ZahidHasan4mJU <responsible.zahid@yahoo.com>
2014-09-28 22:09:45 +0600

    added

---
+++ HashtableSample2/HashtableSample2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashtableSample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable a = new Hashtable();
+            a.Add(111, "fhsfkjshfkjs");
+            a.Add(222, "hskjhsjhf");
+            a.Add(333, "fjhfjdash");
+            string b = (string)a["111"];
+            Console.WriteLine(b);
+           
+            
+            
+            
+            foreach (int k in a.Keys)
+
+            {
+                Console.WriteLine(k);
+                
+
+                
+            }
+            foreach (string v in a.Values)
+            {
+               Console.WriteLine(v);
+                
+            }
+            Console.ReadKey();
+            
+        }
+        
+    }
+}
